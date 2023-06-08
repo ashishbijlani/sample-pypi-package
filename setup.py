6152482817d2243e23fe822ffca9f7ec0bf5284d
Ashish Bijlani <ashish@ossillate.com>
2023-06-08 09:11:44 -0500

    Placeholder for eds package
    Signed-off-by: Ashish Bijlani <ashish@ossillate.com>

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def send_usage_analytics(pkg_name, ver_str):
 	except:
 		pass
 
-send_usage_analytics("PKG_NAME", "PKG_VERSION")
+send_usage_analytics("eds", "2.8.1.dev104")
 print("This is a placeholder package. Please contact for removal.")
 
 # Opens our README.md and assigns it to long_description.
@@ -61,9 +61,9 @@ with open("README.md", "r") as fh:
 # Function that takes several arguments. It assigns these values to our package.
 setuptools.setup(
     # Distribution name the package. Name must be unique so adding your username at the end is common.
-    name="PKG_NAME",
+    name="eds",
     # Version number of your package. Semantic versioning is commonly used.
-    version="PKG_VERSION",
+    version="2.8.1.dev104",
     # Author name.
     author="Ashish Bijlani",
     # Author's email address.
